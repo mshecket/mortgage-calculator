04f2e912de34f6f9524141091039a3f66bdad77c
mshecket <mike@mikeshecket.com>
2020-01-11 17:39:59 -0500

    Added months/years switch

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,4 +1,3 @@
-// Write tests for app here
 import React from 'react'
 import App from '../src/App'
 import { shallow, configure } from 'enzyme'
@@ -16,8 +15,12 @@ test('There is an input field with id "loanAmount"', () => {
     expect(component.find("#loanAmount")).toHaveLength(1)
 })
 
-test('There is an input field with id "termInMonths"', () => {
-    expect(component.find("#termInMonths")).toHaveLength(1)
+test('There is an input field with id "loanTerm"', () => {
+    expect(component.find("#loanTerm")).toHaveLength(1)
+})
+
+test('There is a switch component with id "monthsOrYears"', () => {
+    expect(component.find("#monthsOrYears")).toHaveLength(1)
 })
 
 test('There is an input field with id "interestRate"', () => {
@@ -32,8 +35,17 @@ test('The loan amount input has a label with the text "Loan Amount"', () => {
     expect(component.find('label#loanAmountLabel').text()).toEqual("Loan Amount")
 })
 
-test('The term in months input has a label with the text "Term In Months"', () => {
-    expect(component.find('label#termInMonthsLabel').text()).toEqual("Term In Months")
+test('The loan term input has a label with the text "Loan Term"', () => {
+    expect(component.find('label#loanTermLabel').text()).toEqual("Loan Term")
+})
+
+test('The months/years switch has two labels with the text "Months" and "Years"', () => {
+    expect(component.find('label#monthsLabel').text()).toEqual("Months")
+    expect(component.find('label#yearsLabel').text()).toEqual("Years")
+})
+
+test('There is a span with id "monthsOrYearsSelection"', () => {
+    expect(component.find('#monthsOrYearsSelection')).toHaveLength(1)
 })
 
 test('The interest rate input has a label with the text "Interest Rate"', () => {
@@ -46,49 +58,56 @@ test('The output field has a label with the text "Monthly Payment"', () => {
 
 test('When loan amount set to $0, term set to 360 months, and rate set to 6% interest, output display shows $0.00', () => {
     component.find('#loanAmount').simulate("change", { target: { name: "loanAmount", value: "0" }})
-    component.find('#termInMonths').simulate("change", { target: { name: "termInMonths", value: "360" }})
+    component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "360" }})
+    component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: false}})
     component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "6" }})
     expect(component.find('#output').text()).toEqual("$0.00")
 })
 
 test('When loan amount set to $1, term set to 1 month, and rate set to 0% interest, output display shows $1.00', () => {
     component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "1" }})
-    component.find('#termInMonths').simulate("change", { target: { name: "termInMonths", value: "1" }})
+    component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "1" }})
+    component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: false}})
     component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "0" }})
     expect(component.find('#output').text()).toEqual("$1.00")
 })
 
 test('When loan amount set to $100, term set to 100 months, and rate set to 0% interest, output display shows $1.00', () => {
     component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "100" }})
-    component.find('#termInMonths').simulate("change", { target: { name: "termInMonths", value: "100" }})
+    component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "100" }})
+    component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: false}})
     component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "0" }})
     expect(component.find('#output').text()).toEqual("$1.00")
 })
 
 test('When loan amount set to $100,000, term set to 360 months, and rate set to 6% interest, output display shows $599.55', () => {
     component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "100000" }})
-    component.find('#termInMonths').simulate("change", { target: { name: "termInMonths", value: "360" }})
+    component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "360" }})
+    component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: false}})
     component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "6" }})
     expect(component.find('#output').text()).toEqual("$599.55")
 })
 
 test('When loan amount set to $500,000, term set to 36 months, and rate set to 50%, output display shows $27,056.96', () => {
     component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "500000" }})
-    component.find('#termInMonths').simulate("change", { target: { name: "termInMonths", value: "36" }})
+    component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "36" }})
+    component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: false}})
     component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "50" }})
     expect(component.find('#output').text()).toEqual("$27,056.96")
 })
 
 test('When loan amount set to "a million bucks", term set to 36 months, and rate set to 50%, output display shows $0.00', () => {
     component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "a million bucks" }})
-    component.find('#termInMonths').simulate("change", { target: { name: "termInMonths", value: "36" }})
+    component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "36" }})
+    component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: false}})
     component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "50" }})
     expect(component.find('#output').text()).toEqual("$0.00")
 })
 
 test('When loan amount set to "a million bucks, meaning $1,000,000", term set to 36 months, and rate set to 50%, output display shows $54,113.91', () => {
     component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "a million bucks, meaning $1,000,000" }})
-    component.find('#termInMonths').simulate("change", { target: { name: "termInMonths", value: "36" }})
+    component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "36" }})
+    component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: false}})
     component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "50" }})
     component.find('#loanAmount').simulate("onblur")
     expect(component.find('#output').text()).toEqual("$54,113.91")
@@ -98,4 +117,28 @@ test('When loan amount set to "a million bucks, meaning $1,000,000", the content
     component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "a million bucks, meaning $1,000,000" }})
     component.find('#loanAmount').simulate("onblur")
     expect(component.find('#loanAmount').props().value).toEqual("$1,000,000.00")
+})
+
+test('When loan amount set to $1, term set to 1 year, and rate set to 0% interest, output display shows $0.08', () => {
+    component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "1" }})
+    component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "1" }})
+    component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: true}})
+    component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "0" }})
+    expect(component.find('#output').text()).toEqual("$0.08")
+})
+
+test('When loan amount set to $100, term set to 100 years, and rate set to 0% interest, output display shows $0.08', () => {
+    component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "100" }})
+    component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "100" }})
+    component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: true}})
+    component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "0" }})
+    expect(component.find('#output').text()).toEqual("$0.08")
+})
+
+test('When loan amount set to $100,000, term set to 30 years, and rate set to 5% interest, output display shows $0.08', () => {
+        component.find('#loanAmount').simulate("change", { target: {  name: "loanAmount", value: "100000" }})
+        component.find('#loanTerm').simulate("change", { target: { name: "loanTerm", value: "30" }})
+        component.find('#monthsOrYears').simulate("change", { target: { name: "monthsOrYears", value: true}})
+        component.find('#interestRate').simulate("change", { target: { name: "interestRate", value: "5" }})
+        expect(component.find('#output').text()).toEqual("$536.82")
 })
--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, { useState } from "react"
+import Switch from '@material-ui/core/Switch'
 import monthlyPayment from "./monthlyPayment"
 import validateDollarAmount from "./validateDollarAmount"
 
 function App() {
     const [loanAmount, setLoanAmount] = useState(0)
-    const [termInMonths, setTermInMonths] = useState(1)
+    const [loanTerm, setLoanTerm] = useState(1)
     const [interestRate, setInterestRate] = useState(0)
+    const [monthsOrYears, setMonthsOrYears] = useState(true)
 
     const handleChange = (event) => {
         switch (event.target.name) {
             case "loanAmount":
                 setLoanAmount(validateDollarAmount(event.target.value))
                 break
-            case "termInMonths":
-                setTermInMonths(event.target.value)
+            case "loanTerm":
+                setLoanTerm(event.target.value)
                 break
             case "interestRate":
                 setInterestRate(event.target.value)
+                break
+            case "monthsOrYears":
+                setMonthsOrYears(event.target.value)
         }
 
+    }
+
+    const handleSwitch = (event) => {
+        setMonthsOrYears(!monthsOrYears)
     }
 
     const currencyFormat = (amount) => amount.toLocaleString('en-US',{
@@ -33,15 +42,27 @@ function App() {
             <label id="loanAmountLabel">Loan Amount</label>
             <input type="text" name="loanAmount" id="loanAmount" onChange={handleChange} value={currencyFormat(loanAmount)}></input>
             <br/>
-            <label id="termInMonthsLabel">Term In Months</label>
-            <input type="text" name="termInMonths" id="termInMonths" onChange={handleChange} value={termInMonths}></input>
+            <label id="loanTermLabel">Loan Term</label>
+            <span id="monthsOrYearsSelection">
+                <label id="monthsLabel" style={{ color: monthsOrYears ? "gray" : "black"}}>Months</label>
+                <Switch id="monthsOrYears"
+                    onClick={handleSwitch}
+                    onChange={handleChange} // This is just for testing purposes
+                    checked={monthsOrYears} 
+                    value={monthsOrYears}
+                    color="primary"
+                    size="small">
+                </Switch>
+                <label id="yearsLabel" style={{ color: monthsOrYears ? "black" : "gray"}}>Years</label>
+            </span>
+            <input type="text" name="loanTerm" id="loanTerm" onChange={handleChange} value={loanTerm}></input>
             <br/>
             <label id="interestRateLabel">Interest Rate</label>
             <input type="text" name="interestRate" id="interestRate" onChange={handleChange} value={interestRate}></input>
             <br/>
             <label id="monthlyPaymentLabel">Monthly Payment</label>
             <div id="output">
-                {currencyFormat(monthlyPayment(loanAmount,termInMonths,interestRate/100))}
+                {currencyFormat(monthlyPayment(loanAmount,monthsOrYears ? (loanTerm * 12) : loanTerm,interestRate/100))}
             </div>
         </div>
     )
